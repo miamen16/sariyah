88f0b50605d0c9c16805053d3ad9bd6323cc67cc
Mohamed Ibrahim Ibrahim <31406965+miamen16@users.noreply.github.com>
2026-09-17 11:26:13 +0300

    Add Product Card render template

---
+++ blocks/product-card/render.php
@@ -0,0 +1,12 @@
+<?php
+/** WooCommerce product card. */
+if ( ! function_exists( 'wc_get_product' ) ) {
+    return;
+}
+
+$product_id = (int) get_field( 'product' );
+$product    = $product_id ? wc_get_product( $product_id ) : false;
+
+if ( $product instanceof WC_Product ) {
+    sariyah_render_product_card( $product );
+}
