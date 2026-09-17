d0a199735e55d5f31cd43d27a8cbd3e41bd90f37
Mohamed Ibrahim Ibrahim <31406965+miamen16@users.noreply.github.com>
2026-09-17 14:16:46 +0300

    Audit: improve WooCommerce product card add-to-cart compatibility

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -17,11 +17,26 @@ function sariyah_woocommerce_active(): bool {
 
 function sariyah_render_product_card( WC_Product $product ): void {
     $product_id = $product->get_id();
-    $image      = $product->get_image( 'woocommerce_thumbnail', array( 'loading' => 'lazy' ) );
+    $classes    = 'sariyah-button sariyah-product-card__button';
+    $url        = $product->get_permalink();
+    $label      = __( 'View product', 'sariyah' );
+    $attributes = '';
+
+    if ( $product->is_purchasable() && $product->is_in_stock() && $product->is_type( 'simple' ) ) {
+        $url        = $product->add_to_cart_url();
+        $label      = $product->add_to_cart_text();
+        $classes   .= ' add_to_cart_button ajax_add_to_cart';
+        $attributes  = sprintf(
+            ' data-quantity="1" data-product_id="%d" data-product_sku="%s" aria-label="%s"',
+            $product_id,
+            esc_attr( $product->get_sku() ),
+            esc_attr( $product->add_to_cart_description() )
+        );
+    }
     ?>
     <article class="sariyah-product-card">
         <a class="sariyah-product-card__image" href="<?php echo esc_url( $product->get_permalink() ); ?>" aria-label="<?php echo esc_attr( $product->get_name() ); ?>">
-            <?php echo wp_kses_post( $image ); ?>
+            <?php echo wp_kses_post( $product->get_image( 'woocommerce_thumbnail', array( 'loading' => 'lazy' ) ) ); ?>
             <?php if ( $product->is_on_sale() ) : ?><span class="sariyah-product-card__badge"><?php esc_html_e( 'Sale', 'sariyah' ); ?></span><?php endif; ?>
         </a>
         <div class="sariyah-product-card__body">
@@ -30,11 +45,7 @@ function sariyah_render_product_card( WC_Product $product ): void {
             <?php endif; ?>
             <h3 class="sariyah-product-card__title"><a href="<?php echo esc_url( $product->get_permalink() ); ?>"><?php echo esc_html( $product->get_name() ); ?></a></h3>
             <div class="sariyah-product-card__price"><?php echo wp_kses_post( $product->get_price_html() ); ?></div>
-            <?php if ( $product->is_purchasable() && $product->is_in_stock() && $product->is_type( 'simple' ) ) : ?>
-                <a class="sariyah-button sariyah-product-card__button" href="<?php echo esc_url( $product->add_to_cart_url() ); ?>" data-quantity="1" data-product_id="<?php echo esc_attr( $product_id ); ?>" rel="nofollow"><?php esc_html_e( 'Add to cart', 'sariyah' ); ?></a>
-            <?php else : ?>
-                <a class="sariyah-button sariyah-product-card__button" href="<?php echo esc_url( $product->get_permalink() ); ?>"><?php esc_html_e( 'View product', 'sariyah' ); ?></a>
-            <?php endif; ?>
+            <a class="<?php echo esc_attr( $classes ); ?>" href="<?php echo esc_url( $url ); ?>" rel="nofollow"<?php echo $attributes; ?>><?php echo esc_html( $label ); ?></a>
         </div>
     </article>
     <?php
